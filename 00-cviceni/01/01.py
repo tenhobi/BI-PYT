25c410fc473d88d75712c4256a6b34d6331c839e
HoBi <the@honzabittner.cz>
2017-03-14 09:20:33 +0100

    Add lessons

---
+++ 00-cviceni/01/01.py
@@ -0,0 +1,20 @@
+import time
+from os import get_terminal_size as gts
+import sys
+
+printx = sys.stdout.write
+flushx = sys.stdout.flush
+
+cols, rows = gts()
+cols, rows = cols // 2, rows // 2
+
+printx('\033[2J')
+printx(f'\033[{rows};{cols}H')
+printx('\033[1;35;40m')
+
+while True:
+    for i in range(1, 61):
+        printx(f"{i:2}")
+        flushx()
+        time.sleep(0.1)
+        printx('\b\b')
